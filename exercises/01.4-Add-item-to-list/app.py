66afa437207e25c1919a7915c499ec59b6a3d88c
gleonag <gleonag@gmail.com>
2022-11-15 20:27:43 +0000

    hacer ejercicio 1,4 y 2

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,12 @@
 #Remember import random function here:
+import random
 
 my_list = [4,5,734,43,45]
 
 #The magic is here:
+for i in range(0,10):
+    i = random.randint(0, 1000)
+    print(i)
+
+out_arr = np.add(my_list,i)
+print(out_arr)
